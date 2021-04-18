1a280b0c6603a4f3ce1c8dc98ab59f6890f22925
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-17 23:40:51 -0300

    So executar esse arquivo!

--- main.py
+++ main.py
@@ -9,12 +9,20 @@ arq_jso = open(local, "r")
 # Carregar arquivos json
 arq_jso = json.load(arq_jso)
 
-print("Digite o formato, exemplo '.p'")
+print("Digite o final do formato junto com o ponto, exemplo '.p'")
 
 while True:
-
+    c = 0
+    achou = False
     procura = input("Digite: ")
     for i in range(len(arq_jso)):
+
         if "extensions" in arq_jso[i]:
             if procura in arq_jso[i]["extensions"]:
                 print(arq_jso[i]["name"])
+                achou = True
+
+    # Se não achou
+    if achou != True:
+        print("Não achou nenhum formato.")
+        print("Did not find any format.")
